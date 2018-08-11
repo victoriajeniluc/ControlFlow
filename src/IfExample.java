d24ade7fadef9ec3f3b02c75def773040e3c9872
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-11 08:40:10 -0500

    comment on if statement with curly braces

--- src/IfExample.java
+++ src/IfExample.java
@@ -16,7 +16,7 @@ public class IfExample {
             //System.out.println("Excellent!"); .. this is commented out so I can run the next example with two statements in the if statement
 
 
-            // if there is no curly braces around the two statements... java will run the first one as part of the if statement and the other one as a separate entity. it will not give a compilation error!
+            // if there is no curly braces around the two statements... java will run the first one as part of the if statement and the other one as a separate entity. this will also go for the same thing as putting two of the statements on the same line... as long as there is a  semicolon... the second statement will be ran as a separate entity! it will not give a compilation error for both!
         if (score > 80){
             System.out.println("Excellent!");
             System.out.println("You passed with an A");
