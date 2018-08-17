5138d57389e34d35ebcb0138789a2e83a0bb220e
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-16 22:59:02 -0500

    comment on why it is important for breaks in between cases

--- src/_03Example.java
+++ src/_03Example.java
@@ -10,29 +10,30 @@
 
 public class _03Example {
     public static void main(String[] args) {
-        String colorCode = "p";
+        String colorCode = "g";
 
+        // it is important to put breaks after the cases because it allows the program to exit out of that case.. if you do not add it... it will execute that block perfectly fine (if it is true)... but then it will execute the blocks after that statement until it finds a "break"
         switch(colorCode) {
             default:
                 System.out.println("Not a valid color");
                 break;
             case "R":
-                System.out.println("RED");
+                System.out.println("Traffic color is RED");
                 break;
             case "r":
-                System.out.println("RED");
+                System.out.println("Traffic color is RED");
                 break;
             case "O":
-                System.out.println("ORANGE");
+                System.out.println(" Traffic color is ORANGE");
                 break;
             case "o":
-                System.out.println("ORANGE");
+                System.out.println(" Traffic color is ORANGE");
                 break;
             case "G":
-                System.out.println("GREEN");
+                System.out.println("Traffic color is GREEN");
                 break;
             case "g":
-                System.out.println("green");
+                System.out.println("Traffic color is green");
                 break;
         }
     }
