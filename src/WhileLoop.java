9823b27539ad555fc626dd8b5c374ca5ae47a189
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-18 15:16:51 -0500

    GAVE AN EXAMPLE OF WHILE LOOP

---
+++ src/WhileLoop.java
@@ -0,0 +1,36 @@
+/**
+ * Create a program that will print out Hello 10 times on the console without using any looping
+ * @author Udayan Khattry
+ */
+
+// public class WhileLoop {
+   // public static void main(String[] args) {
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+       // System.out.println("hello");
+
+ //   }
+//}
+
+/**
+ * Create a program that will print hello 10 using a while loop
+ */
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        byte counter = 0;
+
+        while(counter < 10) {
+            System.out.println("hello");
+            counter++;
+        }
+
+    }
+}
